8a6592211ea3f107fba7f53965ebd33c34d28e01
Zesen Qian <qzs123@gmail.com>
2014-01-11 17:01:01 +0800

    * detect incomplete files + README

--- interfacelift-grabber.py
+++ interfacelift-grabber.py
@@ -1,22 +1,15 @@
 #! /usr/bin/env python3
 import argparse
-from urllib import parse
-from urllib import request
-from urllib import response
+from http import client
+from urllib.parse import urlparse
 import datetime
 import os
 import sys
 import re
 from string import Template
 
-class NotModifiedHandler(request.BaseHandler):
-    def http_error_304(self, req, fp, code, msg, hdrs):
-        addinfourl = response.addinfourl(fp, hdrs, req.get_full_url())
-        addinfourl.code = code
-        return addinfourl
-
-opener = request.build_opener(NotModifiedHandler())
-opener.addheaders = [('User-Agent', 'Mozilla/5.0 (X11; Linux i686; rv:24.0) Gecko/20140108 Firefox/24.0')]
+headers = {'User-Agent':'Mozilla/5.0 (X11; Linux i686; rv:24.0) Gecko/20140108 Firefox/24.0'}
+host = 'interfacelift.com'
 
 class SkipException(Exception):
     pass
@@ -37,7 +30,9 @@ class Page:
     pattern = re.compile(r'<select class="select" .*? onChange="javascript:imgload\(\'(?P<base>.*?)\', this,\'(?P<id>\d+)\'\)">\s*(?P<resolution>.*?)\s*</select>.*?</div>\s*</div>\s*</div>\s*<div class="details">\s*<div>\s*<h1 .*?><a .*?>(?P<title>.*?)</a></h1>\s*<div .*?>By <a .*?>(?P<artist>.*?)</a></div>\s*<div .*?>(?P<date>.*?)</div>', re.DOTALL)
     
     def parse(self):
-        self.res = opener.open(self.url)
+        self.conn = client.HTTPConnection(host)
+        self.conn.request('GET', self.url, headers=headers)
+        self.res = self.conn.getresponse()
         for match in re.finditer(Page.pattern, self.res.read().decode('utf-8')):
             date = datetime.datetime.strptime(re.sub(r'(st|nd|rd|th),', ',', match.group('date')), '%B %d, %Y')
             yield {'base':match.group('base'),
@@ -57,50 +52,56 @@ class Wallpaper:
         self.url = url;
 
     def save(self, path):
-        self.req = request.Request(self.url)
+        self.conn = client.HTTPConnection(host)
+        self.conn.request('GET', self.url, headers=headers)
+        self.res = self.conn.getresponse()
+        self.url = urlparse(self.res.getheader('Location')).path
+        self.res.read()
+        self.conn.request('GET', self.url, headers=headers)
+        self.res = self.conn.getresponse()
+        
         try:
-            ltime = round(os.path.getmtime(path))
-            self.localtime = datetime.datetime.utcfromtimestamp(ltime)
-            if not args.force:
-                self.req.add_header('If-Modified-Since', self.localtime.strftime('%a, %d %b %Y %H:%M:%S GMT'))
+            self.localtime = round(os.path.getmtime(path))
+            self.localsize = round(os.path.getsize(path))
+        except os.error:
+            self.localtime = 0
+            self.localsize = 0
+
+        try:
+            time = datetime.datetime.strptime(self.res.getheader('Last-Modified'), '%a, %d %b %Y %H:%M:%S GMT')
+            self.size = int(self.res.getheader('Content-Length'))
+        except TypeError:
+            raise FailException
+        else:
+            self.time = round((time - datetime.datetime(1970, 1, 1)).total_seconds())
+
+
+        if (not args.force and self.localtime == self.time and self.localsize == self.size):
+            self.conn.close()
+            raise SkipException
+            
+        try:
+            os.makedirs(os.path.dirname(path))
         except os.error:
             pass
 
-        self.res = opener.open(self.req)
-
-        if hasattr(self.res, 'code') and self.res.code == 304:
-            raise SkipException
-        else:
-            try:
-                self.remotetime = datetime.datetime.strptime(self.res.getheader('Last-Modified'), '%a, %d %b %Y %H:%M:%S GMT')
-                self.length = int(self.res.getheader('Content-Length'))
-            except TypeError:
-                raise FailException
-            mtime = round((self.remotetime - datetime.datetime(1970, 1, 1)).total_seconds())
-            #make directory if needed
-            try:
-                os.makedirs(os.path.dirname(path))
-            except os.error:
-                pass
-            self.readed = 0;
-            with open(path, 'wb') as f:
-                while self.readed < self.length:
-                    buffer = self.res.read(int(self.length / 100))
-                    f.write(buffer)
-                    self.readed += len(buffer)
-                    print('{0:6d}%'.format(int(self.readed * 100 / self.length)), end='\r')
-            #set modified time
-            os.utime(path, (mtime, mtime))
-            if 'ltime' in locals():
-                raise ForceException
+        self.lsize = 0;
+        with open(path, 'wb') as f:
+            while self.lsize < self.size:
+                buffer = self.res.read(int(self.size / 100))
+                f.write(buffer)
+                self.lsize += len(buffer)
+                print('{0:6d}%'.format(int(self.lsize * 100 / self.size)), end='\r')
+        os.utime(path, (self.time, self.time))
+        if (args.force and self.localtime == self.time and self.localsize == self.size):
+            raise ForceException
         
-parser = argparse.ArgumentParser(description='Download wallpaper from interfacelift',
-                                 formatter_class=argparse.RawTextHelpFormatter)
+parser = argparse.ArgumentParser(description='Download wallpaper from interfacelift', formatter_class=argparse.RawTextHelpFormatter)
 parser.add_argument('width', type=int, help='The width of wallpapers')
 parser.add_argument('height', type=int, help='The height of wallpapers')
 parser.add_argument('-t', '--template', default='$base', help='Format of saved path\n${base}=northerncastle\n${title}=Northern Castle\n${id}=03467\n${artist}=Nicolas Kamp\n${year}=2014\n${month}=01\n${MONTH}=Jan\n${day}=09')
 parser.add_argument('-l', '--limit', default=-1, type=int, help='Number of wallpapers to download(default: %(default)s)')
-parser.add_argument('-f', '--force', action='store_const', const=True, default=False, help='Overwrite existed wallpapers')
+parser.add_argument('-f', '--force', action='store_const', const=True, default=False, help='Do not skip, overwrite existed wallpapers even if timestamp and size is correct')
 parser.add_argument('-q', '--quick', action='store_const', const=True, default=False, help='Quit on first skip')
 parser.add_argument('--date', dest='sort', action='store_const', const="date", default='date', help='Sort by date(default)')
 parser.add_argument('--downloads', dest='sort', action='store_const', const="downloads", default='date', help='Sort by downloads')
@@ -119,7 +120,7 @@ try:
     while True:
         if count == args.limit:
             break
-        page = Page("http://interfacelift.com/wallpaper/downloads/%s/any/index%d.html" % (args.sort, page_number));
+        page = Page("/wallpaper/downloads/%s/any/index%d.html" % (args.sort, page_number));
         page_number += 1
         for item in page.parse():
             empty = False
@@ -130,7 +131,7 @@ try:
                 item['width'] = int(args.width)
                 item['height'] = int(args.height)
                 path = template.substitute(item)
-                url = parse.urljoin(page.url, '/wallpaper/7yz4ma1/%s_%s_%dx%d.jpg' % (item['id'], item['base'], args.width, args.height))
+                url = '/wallpaper/7yz4ma1/%s_%s_%dx%d.jpg' % (item['id'], item['base'], args.width, args.height)
                 if path + url in paths:
                     continue
                 else:
@@ -157,3 +158,6 @@ try:
             raise QuitException
 except QuitException:
     pass
+finally:
+    print()
+
